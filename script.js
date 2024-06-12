3ceba9ccb7d2c25bdd5c6cfa2262d27b3753705e
biswass101 <biswassnaeemcse@gmail.com>
2024-06-12 15:44:38 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// fetch('https://jsonplaceholder.typicode.com/todos/1').then(response => response.json()).then(json => console.log(json))
+
+
+//fetching data uisng axios-->
+import axios from 'axios'
+axios.get('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => console.log(response.data))
